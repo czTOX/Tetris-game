f2bb35f477de6c38f6c827a0fa7d64c4ccc9975c
cztox <navrkalt29@gmail.com>
2020-09-29 19:34:00 +0200

    piece rotation

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import pygame
 
 
-def drawBoard(window, x, y, height, width, squeare):
+def drawBoard(window, x, y, height, width, squeare, board):
     #Constants
     background = [105,105,105]
     black = [0, 0, 0]
@@ -18,9 +18,28 @@ def drawBoard(window, x, y, height, width, squeare):
     for j in range(20):
         pygame.draw.line(window, [0, 0, 0], (x, y+j*squeare), (x+width, y+j*squeare), 1)
 
+    #Draw all 1 in a 2D array
+    for i in range(10):
+        for j in range(20):
+            if board[i][j] == 1:
+                drawSquere(window, 200+i*squeare, 50+j*squeare, squeare, [255, 255, 255])
+
+
     pygame.display.update()
 
 
 def drawSquere(window, x, y, squeare, color):
     pygame.draw.rect(window, color, (x+1, y+1, squeare-1, squeare-1))
     pygame.display.update()
+
+#TODO need to rework for a whole board
+def replaceWithZero(board):
+    for i in range(4):
+        for j in range(4):
+            board[3+i][j] = 0
+
+#TODO replace at any place on the board
+def replaceWithShape(board, shape, stageOfTheShape, startPoint = 0):
+    for i in range(4):
+        for j in range(4):
+            board[3+i][j] = shape.structure[stageOfTheShape][4*i+j]
--- main.py
+++ main.py
@@ -27,26 +27,36 @@ shapeArray = [shapeO, shapeI, shapeS, shapeZ, shapeL, shapeJ, shapeT]
 pygame.init()
 window = pygame.display.set_mode((width, height))
 pygame.draw.rect(window, [253, 223, 179], (0, 0, width, height))
-fn.drawBoard(window, 200, 50, 600, 300, squeare)
+fn.drawBoard(window, 200, 50, 600, 300, squeare, board)
 pygame.display.update()
 
 shape = random.choice(shapeArray)
+stageOfTheShape = 0
 
 
 #Replace chunk of 2D board with a shape block
 for i in range(4):
     for j in range(4):
-        board[3+i][j] = shape.structure[4*i+j]
+        board[3+i][j] = shape.structure[stageOfTheShape][4*i+j]
 
+fn.drawBoard(window, 200, 50, 600, 300, squeare, board)
 
-#Draw all 1 in a 2D array
-for i in range(10):
-    for j in range(20):
-        if board[i][j] == 1:
-            fn.drawSquere(window, 200+i*squeare, 50+j*squeare, squeare, [255, 255, 255])
 
 #Main loop
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                lenght = len(shape.structure)-1
+                if stageOfTheShape < lenght:
+                    stageOfTheShape+=1
+                elif stageOfTheShape == lenght:
+                    stageOfTheShape = 0
+                fn.replaceWithZero(board)
+                fn.replaceWithShape(board, shape, stageOfTheShape)
+
+                fn.drawBoard(window, 200, 50, 600, 300, squeare, board)
+
